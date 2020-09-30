4a5c24c7b8f3bbb386b3c456e7a38c6d87277b20
Ernst Pierre <emmp318@gmail.com>
2020-09-29 22:46:19 -0500

    modified projects.jsx

---
+++ src/component/projects.jsx
@@ -0,0 +1,250 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class Projects extends React.Component {
+
+	render() {
+		return (
+			<React.Fragment>
+				<div id="my-projects" className="container-fluid bg-light mt-5">
+					<div className=" project-container container bg-light pb-5">
+						<h1
+							id="projects"
+							className={"text-center " + this.props.fadeInRight}>
+							Projects
+						</h1>
+						<div className="row my-5">
+							<div
+								className={
+									"testbg col-12 col-sm-12 col-md-4 " +
+									this.props.fadeIn
+								}>
+								<div className="overlay">
+									<div className="text">
+										<h2></h2>
+									</div>
+									<div>
+										<button
+											id="vmarine-button"
+											className="project-button"
+											onClick={() => {
+												var modalBG = document.getElementById(
+													"gallery-card"
+												);
+												var marineModal = document.getElementById(
+													"vmarine"
+												);
+												modalBG.style.display = "block";
+												marineModal.style.display =
+													"block";
+											}}>
+											Learn More
+										</button>
+									</div>
+								</div>
+							</div>
+							<div
+								className={
+									"aguabg col-12 col-sm-12 col-md-4 " +
+									this.props.fadeIn
+								}>
+								<div className="overlay">
+									<div className="text">
+										<h2></h2>
+									</div>
+									<div>
+										<button
+											id="agua-button"
+											className="project-button"
+											onClick={() => {
+												var modalBG = document.getElementById(
+													"gallery-card"
+												);
+												var aguaModal = document.getElementById(
+													""
+												);
+												modalBG.style.display = "block";
+												aguaModal.style.display =
+													"block";
+											}}>
+											Learn More
+										</button>
+									</div>
+								</div>
+							</div>
+							<div
+								className={
+									"todobg col-12 col-sm-12 col-md-4 " +
+									this.props.fadeIn
+								}>
+								<div className="overlay">
+									<div className="text">
+										<h2></h2>
+									</div>
+									<div>
+										<button
+											id="todo-button"
+											className="project-button"
+											onClick={() => {
+												var modalBG = document.getElementById(
+													"gallery-card"
+												);
+												var todoModal = document.getElementById(
+													""
+												);
+												modalBG.style.display = "block";
+												todoModal.style.display =
+													"block";
+											}}>
+											Learn More
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				{/*hidden modals*/}
+				<div id="gallery-card">
+					<div id="vmarine" className="modal-card">
+						<div className="visual">
+						</div>
+						<div className="modal-info">
+							<h2>Vmarine</h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										
+									</li>
+									<li>
+										
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<h4>Comming Soon!</h4>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var marineModal = document.getElementById(
+											"v"
+										);
+										modalBG.style.display = "none";
+										marineModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div id="agualuz" className="modal-card">
+						<div className="visual">
+						</div>
+						<div className="modal-info">
+							<h2></h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										
+									</li>
+									<li>
+										
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<a
+									href=""
+									target="_blank">
+									<h3></h3>
+								</a>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var aguaModal = document.getElementById(
+											""
+										);
+										modalBG.style.display = "none";
+										aguaModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div id="todolist" className="modal-card">
+						<div className="visual">
+						</div>
+						<div className="modal-info">
+							<h2></h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										
+									</li>
+									<li>
+										
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<a href="" target="_blank">
+									<h3>View live demo</h3>
+								</a>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var todoModal = document.getElementById(
+											"todolist"
+										);
+										modalBG.style.display = "none";
+										todoModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+Projects.propTypes = {
+	bounceIn: PropTypes.string,
+	fadeInLeft: PropTypes.string,
+	fadeInRight: PropTypes.string,
+	fadeIn: PropTypes.string,
+	tada: PropTypes.string
+};
