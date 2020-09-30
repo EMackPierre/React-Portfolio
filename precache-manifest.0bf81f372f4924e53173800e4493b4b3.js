f6ac39a9191116b1bba9305e0982e916817389d7
Ernst Pierre <emmp318@gmail.com>
2020-09-29 22:57:53 -0500

    Updates

---
+++ precache-manifest.0bf81f372f4924e53173800e4493b4b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86b63068902ac801c5b169e0845a6b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f425a6d19db00468a612",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "71fe8100e4f7faf5f7be",
+    "url": "/static/css/main.70f50a14.chunk.css"
+  },
+  {
+    "revision": "f425a6d19db00468a612",
+    "url": "/static/js/2.239b12ea.chunk.js"
+  },
+  {
+    "revision": "71fe8100e4f7faf5f7be",
+    "url": "/static/js/main.bab7963c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a681d1761588bb7b8e25c8ef58c104d5",
+    "url": "/static/media/myImage.a681d176.jpg"
+  }
+]);
