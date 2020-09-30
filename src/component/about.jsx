efa98e3ff4ed2292f3e04d4afca6a3cddd1a7213
Ernst Pierre <emmp318@gmail.com>
2020-09-29 22:15:59 -0500

    added jpg and png files

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -3,7 +3,7 @@ import layers from "../img/layers.png";
 import cogwheel from "../img/cogwheel.png";
 import pen from "../img/pen.png";
 import browser from "../img/browser.png";
-import myImage from "../img/myImage.png";
+import myImage from "../img/myImage.jpg";
 import PropTypes from "prop-types";
 
 export default class About extends React.Component {
